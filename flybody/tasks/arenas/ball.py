ebe9b4b805f7e4a85627eba17258eaa6948f2ac2
Roman Vaxenburg <vaxenburg@gmail.com>
2024-02-07 13:32:38 -0500

    Add floating ball arena and task.

---
+++ flybody/tasks/arenas/ball.py
@@ -0,0 +1,65 @@
+"""Floating ball arena."""
+
+from dm_control import composer
+
+
+class BallFloor(composer.Arena):
+    """A floating ball arena for tethered fruitfly walking tasks."""
+
+    def _build(self, ball_pos=(0, 0, 0), ball_radius=1, ball_density=1.,
+               name='ball', skybox=False, reflectance=0.2,
+               top_camera_y_padding_factor=1.1,
+               top_camera_distance=100):
+
+        super()._build(name=name)
+        self._size = ball_radius
+
+        self._mjcf_root.visual.headlight.set_attributes(
+            ambient=[.4, .4, .4],
+            diffuse=[.8, .8, .8],
+            specular=[.1, .1, .1],)
+
+        self._ground_texture = self._mjcf_root.asset.add(
+            'texture',
+            rgb1=[.2, .3, .4],
+            rgb2=[.1, .2, .3],
+            type='2d',
+            builtin='checker',
+            name='groundplane',
+            width=200,
+            height=200,
+            mark='edge',
+            markrgb=[1., 1., 1.],)
+        self._ground_material = self._mjcf_root.asset.add(
+            'material',
+            name='groundplane',
+            texrepeat=[2, 2],  # Makes white squares exactly 1x1 length units.
+            texuniform=True,
+            reflectance=reflectance,
+            texture=self._ground_texture,)
+        if skybox:
+            self._skybox_texture = self._mjcf_root.asset.add(
+                'texture', type='skybox', name='skybox', builtin='gradient',
+                rgb1=[.4, .6, .8], rgb2=[0., 0., 0.], markrgb=[1, 1, 1],
+                mark='random', random=0.02, width=800, height=800,)
+        
+        # Build ball.
+        ball = self._mjcf_root.worldbody.add('body', name='ball', pos=ball_pos)
+        self._ground_geom = ball.add(
+            'geom',
+            type='sphere',
+            size=(ball_radius, 0, 0),
+            material=self._ground_material,
+            density=ball_density)
+        self._ball_joint = ball.add('joint', name='ball', type='ball')
+        
+    @property
+    def ground_geoms(self):
+        return (self._ground_geom,)
+
+    def regenerate(self, random_state):
+        pass
+
+    @property
+    def size(self):
+        return self._size
---
+++ flybody/tasks/walk_on_ball.py
@@ -0,0 +1,94 @@
+"""Task of tethered fly walking on floating ball."""
+
+from typing import Optional
+import numpy as np
+
+from dm_control import composer
+from dm_control.composer.observation import observable
+from dm_control.utils import rewards
+
+from flybody.tasks.base import Walking
+from flybody.tasks.task_utils import retract_wings
+from flybody.tasks.constants import (
+  _TERMINAL_ANGVEL, 
+  _TERMINAL_LINVEL)
+
+
+class WalkOnBall(Walking):
+  """Tethered fly walking on floating ball."""
+
+  def __init__(self,
+               claw_friction: Optional[float] = 1.0,
+               **kwargs):
+    """Task of tethered fly walking on floating ball.
+
+    Args:
+      claw_friction: Friction of claw geoms with floor.
+      **kwargs: Arguments passed to the superclass constructor.
+    """
+
+    super().__init__(add_ghost=False, ghost_visible_legs=False, **kwargs)
+    
+    # Fuse fly's thorax with world.
+    self.root_entity.mjcf_model.find(
+        'attachment_frame', 'walker').freejoint.remove()
+
+    # Exclude "surprising" thorax-children collisions.
+    for child in self._walker.mjcf_model.find('body', 'thorax').all_children():
+        if child.tag == 'body':
+            self._walker.mjcf_model.contact.add(
+                'exclude', name=f'thorax_{child.name}',
+                body1='thorax', body2=child.name)
+
+    # Maybe change default claw friction.
+    if claw_friction is not None:
+      self._walker.mjcf_model.find(
+        'default', 'adhesion-collision').geom.friction = (claw_friction,)
+    
+    # Enable task-specific observables.
+    self._walker.observables.add_observable('ball_qvel', self.ball_qvel)
+    
+    
+  def initialize_episode_mjcf(self, random_state: np.random.RandomState):
+    super().initialize_episode_mjcf(random_state)
+    # Maybe do something here.
+
+  def initialize_episode(self, physics: 'mjcf.Physics',
+                         random_state: np.random.RandomState):
+    """Randomly selects a starting point and set the walker."""
+    super().initialize_episode(physics, random_state)
+    # Retract wings.
+    retract_wings(physics)
+
+  def before_step(self, physics: 'mjcf.Physics', action,
+                  random_state: np.random.RandomState):
+    # Maybe do something here.
+    super().before_step(physics, action, random_state)
+
+  def get_reward_factors(self, physics):
+    """Returns factorized reward terms."""
+    
+    ball_qvel = physics.named.data.qvel['ball']
+    target_ball_qvel = [0., -2, 0]
+    qvel = rewards.tolerance(ball_qvel - target_ball_qvel,
+                             bounds=(0, 0),
+                             sigmoid='linear',
+                             margin=2.5,
+                             value_at_margin=0.0)
+    return np.hstack(qvel)
+
+  def check_termination(self, physics: 'mjcf.Physics') -> bool:
+    """Check various termination conditions."""
+    linvel = np.linalg.norm(
+        self._walker.observables.velocimeter(physics))
+    angvel = np.linalg.norm(self._walker.observables.gyro(physics))
+    return (linvel > _TERMINAL_LINVEL
+            or angvel > _TERMINAL_ANGVEL
+            or super().check_termination(physics))
+
+  @composer.observable
+  def ball_qvel(self):
+    """Simple observable of ball rotational velocity."""
+    def get_ball_qvel(physics: 'mjcf.Physics'):
+      return physics.named.data.qvel['ball']
+    return observable.Generic(get_ball_qvel)
