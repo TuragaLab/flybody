ad14f38d2815902a4f3e3e95d78e4e908981da1c
vaxenburgr <vaxenburgr@f14u25.int.janelia.org>
2024-02-29 16:37:40 -0500

    Create tf_utils module.

---
+++ flybody/agents/tf_utils.py
@@ -0,0 +1,28 @@
+"""Utilities for tensorflow networks and nested data structures."""
+
+from acme.tf import utils as tf2_utils
+
+
+class TestPolicyWrapper():
+    """At test time, wraps policy to work with non-batched observations.
+    Works with distributional policies, e.g. trained with the DMPO agent."""
+    
+    def __init__(self, policy, sample=False):
+        """
+        Args:
+            policy: Test policy, e.g. trained policy loaded as 
+                policy = tf.saved_model.load('path/to/snapshot').
+            sample: Whether to return sample or mean of the distribution.
+        """
+        self._policy = policy
+        self._sample = sample
+        
+    def __call__(self, observation):
+        batched_observation = tf2_utils.add_batch_dim(observation)
+        distribution = self._policy(batched_observation)
+        if self._sample:
+            action = distribution.sample()
+        else:
+            action = distribution.mean()
+        action = action[0, :].numpy()  # Remove batch dimension.
+        return action
