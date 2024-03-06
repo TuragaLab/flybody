4762533340f36b6e7e59c36e48e11a4979044077
Roman Vaxenburg <vaxenburg@gmail.com>
2024-03-06 13:04:17 -0500

    Update test.

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,36 +4,37 @@ import numpy as np
 from flybody.fly_envs import walk_on_ball
 
 
-obs = ['accelerometer',
-       'actuator_activation',
-       'appendages_pos',
-       'force',
-       'gyro',
-       'joints_pos',
-       'joints_vel',
-       'touch',
-       'velocimeter',
-       'world_zaxis',
-       'ball_qvel']
+obs_names = ['accelerometer',
+             'actuator_activation',
+             'appendages_pos',
+             'force',
+             'gyro',
+             'joints_pos',
+             'joints_vel',
+             'touch',
+             'velocimeter',
+             'world_zaxis',
+             'ball_qvel']
 
-obs = ['walker/' + s for s in obs]
+obs_names = ['walker/' + s for s in obs_names]
 
 def test_can_create_and_run_environment():
 
     env = walk_on_ball()
     
     obs_spec = env.observation_spec()
-    assert list(obs_spec) == obs
+    assert list(obs_spec) == obs_names
     
     n_act = env.action_spec().shape
     assert n_act == (59,)
 
     timestep = env.reset()
-    assert all(timestep.observation['walker/ball_qvel'] == [0, 0, 0])
+    for name in obs_names:
+        observation = timestep.observation[name]
+        assert isinstance(observation, (float, np.ndarray))
 
     for _ in range(100):
         action = np.random.uniform(-1, 1, n_act)
         timestep = env.step(action)
         assert timestep.reward is not None
 
-    # _ = env.physics.render()
