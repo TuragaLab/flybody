be918b0d8b60a28e4e5d9c451ff780f3575b71d5
Roman Vaxenburg <vaxenburg@gmail.com>
2024-03-06 13:52:15 -0500

    Add flywalker test.

---
+++ tests/test_flywalker.py
@@ -0,0 +1,28 @@
+"""Test python fruitfly walker FruitFly."""
+
+import numpy as np
+from dm_control import mjcf
+
+from flybody.fruitfly.fruitfly import FruitFly
+
+
+obs_names = ['thorax_height', 'abdomen_height', 'world_zaxis_hover',
+             'world_zaxis', 'world_zaxis_abdomen', 'world_zaxis_head',
+             'force', 'touch', 'accelerometer', 'gyro', 'velocimeter',
+             'actuator_activation', 'appendages_pos', 'right_eye', 'left_eye']
+
+
+def test_can_compile_and_step_simulation():
+    fly = FruitFly()
+    physics = mjcf.Physics.from_mjcf_model(fly.mjcf_model)
+    for _ in range(100):
+        physics.step()
+
+def test_evaluate_observables():
+    fly = FruitFly()
+    physics = mjcf.Physics.from_mjcf_model(fly.mjcf_model)
+    for name in obs_names:
+        observable = getattr(fly.observables, name)
+        observation = observable(physics)
+        assert isinstance(observation, (float, np.ndarray))
+
