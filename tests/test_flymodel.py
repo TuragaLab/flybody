291b38181c227a46af664762930720120b346c67
Roman Vaxenburg <vaxenburg@gmail.com>
2024-03-06 12:09:03 -0500

    Update flymodel test.

--- tests/test_flymodel.py
+++ tests/test_flymodel.py
@@ -9,23 +9,38 @@ from dm_control import mjcf
 xml_path = os.path.join(os.path.dirname(__file__),
                         '../flybody/fruitfly/assets/fruitfly.xml')
 
-def test_flymodel():
+expect = {
+    'nq': 109,  # Generalized coordinates.
+    'nv': 108,  # DoFs.
+    'nu': 78,  # Actuators.
+    'nbody': 68,  # Bodies.
+    'njnt': 103,  # Joints.
+    'ngeom': 159,  # Geoms.
+    'nsensor': 15,  # Sensors.
+    'nsensordata': 33,  # Sensor readings.
+    'nsite': 15,  # Sites.
+    'nmesh': 85,  # Meshes.
+    'ntendon': 8,  # Tendons.
+    'neq': 0,  # Equality constraints.
+}
+
+expect_close = {
+    'fly_mass': 0.0009846214672177625,  # Total fly mass.
+}
+
+
+def test_model_parameters():
 
     mjcf_model = mjcf.from_path(xml_path)
     physics = mjcf.Physics.from_mjcf_model(mjcf_model)
 
-    assert physics.model.nq == 109  # Generalized coordinates.
-    assert physics.model.nv == 108  # DoFs.
-    assert physics.model.nu == 78  # Actuators.
-    assert physics.model.nbody == 68  # Bodies.
-    assert physics.model.njnt == 103  # Joints.
-    assert physics.model.ngeom == 159  # Geoms.
-    assert physics.model.nsensordata == 33  # Sensor readings.
-    assert physics.model.nsite == 15  # Sites.
-    assert physics.model.nmesh == 85  # Meshes.
+    for k, v in expect.items():
+        assert getattr(physics.model, k) == v
+
     assert np.isclose(
         physics.named.model.body_subtreemass['thorax'],
-        0.0009846214672177625)  # Total fly mass.
+        expect_close['fly_mass'])
+
 
 def test_can_compile_and_step_simulation():
 
@@ -36,6 +51,6 @@ def test_can_compile_and_step_simulation():
     for _ in range(100):
         physics.data.ctrl[:] = np.random.uniform(-.2, .2, physics.model.nu)
         physics.step()
+        assert isinstance(physics.data.sensordata, np.ndarray)
 
-    # _ = physics.render()
 
