3547c5f95ff5a8578c76abc5893828912da403b6
Roman Vaxenburg <vaxenburg@gmail.com>
2024-03-11 16:59:07 -0400

    Add dev extra.

--- setup.py
+++ setup.py
@@ -28,11 +28,18 @@ tf_requirements = [
     'dm-acme[tf,envs,jax]',
     'tensorflow==2.8.0',
     'tensorflow-probability==0.16.0',
-    'dm-reverb==0.7.0'
+    'dm-reverb==0.7.0',
 ]
 
 ray_requirements = tf_requirements + [
     'ray[default]',
+]
+
+dev_requirements = [
+    'yapf',
+    'ruff',
+    'jupyterlab',
+    'tqdm',
 ]
 
 setup(
@@ -46,7 +53,8 @@ setup(
     install_requires=core_requirements,
     extras_require={
         'tf': tf_requirements,
-        'ray': ray_requirements,  
+        'ray': ray_requirements,
+        'dev': dev_requirements,
     },
     author='Roman Vaxenburg, Yuval Tassa, Zinovia Stefanidi',
     description='MuJoCo fruit fly body model and reinforcement learning tasks',
